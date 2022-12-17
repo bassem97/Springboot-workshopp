f9a00e190a0c792e2658a1e1211ce0e7fe1a882d
BASSEM <bassem.jadoui@esprit.tn>
2022-12-17 22:36:13 +0100

    add cross origin to localhost:4200 ✅

--- src/main/java/com/gdsc/springbootworkshop/controllers/ClassroomController.java
+++ src/main/java/com/gdsc/springbootworkshop/controllers/ClassroomController.java
@@ -12,6 +12,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("classrooms")
+@CrossOrigin(origins = "http://localhost:4200")
 public class ClassroomController {
 
     @Autowired
--- src/main/java/com/gdsc/springbootworkshop/controllers/StudentController.java
+++ src/main/java/com/gdsc/springbootworkshop/controllers/StudentController.java
@@ -11,6 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("students")
+@CrossOrigin(origins = "http://localhost:4200")
 public class StudentController {
 
     @Autowired
